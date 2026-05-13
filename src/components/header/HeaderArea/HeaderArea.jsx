e57d3ecdc74b540a4a3f791dac4647aff93b8597
yoshida9029 <d90290711@gmail.com>
2026-05-13 16:30:58 +0900

    2回目のコミット

--- src/components/header/HeaderArea/HeaderArea.jsx
+++ src/components/header/HeaderArea/HeaderArea.jsx
@@ -1,13 +1,13 @@
-// import styles from './HeaderArea/HeaderArea.module.css';
+import styles from "../HeaderArea/HeaderArea.module.css"
 
 export default function HeaderArea(){
     return(
-        <header className='header'>
+        <header className={styles.header}>
             <img 
-            className="logoBlack"
+            className='logo'
             src="../images/bamos_logo/bamosDesignLogo_black.svg"
             alt="ロゴ"/>
-            <ul className="menuBer">
+            <ul className={styles.menuBer}>
                 <nav>MENU-01</nav>
                 <nav>MENU-02</nav>
                 <nav>MENU-03</nav>
--- src/components/main/MainArea.jsx
+++ src/components/main/MainArea.jsx
@@ -1,5 +1,14 @@
+import styles from "../main/MainArea.module.css"
 export default function MainArea(){
     return(
-        <h1>メイン</h1>
+        <>
+        <div className={styles.sec1}>
+        <img className={styles.first} 
+        src="../images/karsten-winegeart-T-yAoikgido-unsplash.jpg" 
+        alt="ファーストビュー" />
+        <img className={styles.secLogo} src="../images/bamos_logo/bamosDesignLogo_white.svg" alt="" />
+        </div>
+        <h2 className={styles.title}>Latest Blog Posts</h2>
+        </>
     )
 }
